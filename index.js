bd9b84f81a832760fad8c7ef33fab99219b5177a
Andrew Osheroff <andrewosh@gmail.com>
2021-07-19 14:06:37 +0200

    Add uint32 (#2)

--- index.js
+++ index.js
@@ -51,6 +51,22 @@ exports.uint16 = {
   }
 }
 
+exports.uint32 = {
+  preencode (state, n) {
+    state.end += 4
+  },
+  encode (state, n) {
+    state.buffer[state.start++] = n
+    state.buffer[state.start++] = n >>> 8
+    state.buffer[state.start++] = n >>> 16
+    state.buffer[state.start++] = n >>> 24
+  },
+  decode (state) {
+    if (state.end - state.start < 4) throw new Error('Out of bounds')
+    return state.buffer[state.start++] + state.buffer[state.start++] * 256 + state.buffer[state.start++] * 65536 + state.buffer[state.start++] * 16777216
+  }
+}
+
 exports.buffer = {
   preencode (state, b) {
     if (b) {
