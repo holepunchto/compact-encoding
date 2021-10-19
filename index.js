d6339e89b28f6e636eb4aa0d9e4fb4b97dde6f09
Kasper Isager Dalsgarð <kasperisager@hey.com>
2021-10-19 10:34:36 +0200

    `String.codePointAt()` is pretty slow 😕

--- index.js
+++ index.js
@@ -423,13 +423,22 @@ function zigzagEncode (n) {
 function byteLength (string) {
   let length = 0
 
-  for (const char of string) {
-    const code = char.codePointAt(0)
+  for (let i = 0, n = string.length; i < n; i++) {
+    const code = string.charCodeAt(i)
+
+    if (code >= 0xd800 && code <= 0xdbff && i + 1 < n) {
+      const code = string.charCodeAt(i + 1)
+
+      if (code >= 0xdc00 && code <= 0xdfff) {
+        length += 4
+        i++
+        continue
+      }
+    }
 
     if (code <= 0x7f) length += 1
     else if (code <= 0x7ff) length += 2
-    else if (code <= 0xffff) length += 3
-    else length += 4
+    else length += 3
   }
 
   return length
