50050dcd515218f78a1c1f947e7ba385333d1cab
Kasper Isager Dalsgarð <kasperisager@hey.com>
2021-10-27 13:17:22 +0200

    Add 64 bit uint/int encoders and refactor things

--- README.md
+++ README.md
@@ -74,21 +74,30 @@ const bool = cenc.decode(cenc.bool, buf)
 The following encodings are bundled as they are primitives that can be used
 to build others on top. Feel free to PR more that are missing.
 
-* `cenc.uint` - Encodes a uint using [compact-uint](https://github.com/mafintosh/compact-uint)
-* `cenc.int` - Encodes an int using [compact-uint](https://github.com/mafintosh/compact-uint) as a signed int using ZigZag encoding.
-* `cenc.uint16` - Encodes a fixed size uint16 (useful for things like ports)
-* `cenc.uint24` - Encodes a fixed size uint24 (useful for message framing)
-* `cenc.uint32` - Encodes a fixes size uint32 (useful for very large message framing)
-* `cenc.buffer` - Encodes a buffer with it's length uint prefixed. When decoding an empty buf, null is returned.
-* `cenc.raw` - Pass through encodes a buffer - ie a basic copy.
-* `cenc.uint32array` - Encodes a uint32array with it's element length uint32 prefixed.
+* `cenc.uint` - Encodes a uint using [compact-uint](https://github.com/mafintosh/compact-uint).
+* `cenc.uint8` - Encodes a fixed size uint8.
+* `cenc.uint16` - Encodes a fixed size uint16. Useful for things like ports.
+* `cenc.uint24` - Encodes a fixed size uint24. Useful for message framing.
+* `cenc.uint32` - Encodes a fixed size uint32. Useful for very large message framing.
+* `cenc.uint64` - Encodes a fixed size uint64.
+* `cenc.int` - Encodes an int using `cenc.uint` with ZigZag encoding.
+* `cenc.int8` - Encodes a fixed size int8 using `cenc.uint8` with ZigZag encoding.
+* `cenc.int16` - Encodes a fixed size int16 using `cenc.uint16` with ZigZag encoding.
+* `cenc.int24` - Encodes a fixed size int24 using `cenc.uint24` with ZigZag encoding.
+* `cenc.int32` - Encodes a fixed size int32 using `cenc.uint32` with ZigZag encoding.
+* `cenc.int64` - Encodes a fixed size int64 using `cenc.uint64` with ZigZag encoding.
+* `cenc.float32` - Encodes a fixed size float32.
+* `cenc.float64` - Encodes a fixed size float64.
+* `cenc.buffer` - Encodes a buffer with its length uint prefixed. When decoding an empty buffer, `null` is returned.
+* `cenc.raw` - Pass through encodes a buffer, i.e. a basic copy.
+* `cenc.uint32array` - Encodes a uint32array with its element length uint32 prefixed.
 * `cenc.bool` - Encodes a boolean as 1 or 0.
 * `cenc.string` - Encodes a utf-8 string, similar to buffer.
 * `cenc.fixed32` - Encodes a fixed 32 byte buffer.
 * `cenc.fixed64` - Encodes a fixed 64 byte buffer.
 * `cenc.fixed(n)` - Makes a fixed sized encoder.
 * `cenc.array(enc)` - Makes an array encoder from another encoder. Arrays are uint prefixed with their length.
-* `cenc.from(enc)` - Makes a compact encoder from a [codec](https://github.com/mafintosh/codecs) or [abstract-encoding](https://github.com/mafintosh/abstract-encoding)
+* `cenc.from(enc)` - Makes a compact encoder from a [codec](https://github.com/mafintosh/codecs) or [abstract-encoding](https://github.com/mafintosh/abstract-encoding).
 
 ## License
 
--- index.js
+++ index.js
@@ -12,34 +12,41 @@ const uint = exports.uint = {
     state.end += n <= 0xfc ? 1 : n <= 0xffff ? 3 : n <= 0xffffffff ? 5 : 9
   },
   encode (state, n) {
-    if (n <= 0xfc) state.buffer[state.start++] = n
-    else if (n <= 0xffff) encode16(state, n)
-    else if (n <= 0xffffffff) encode32(state, n)
-    else encode64(state, n)
+    if (n <= 0xfc) uint8.encode(state, n)
+    else if (n <= 0xffff) {
+      state.buffer[state.start++] = 0xfd
+      uint16.encode(state, n)
+    } else if (n <= 0xffffffff) {
+      state.buffer[state.start++] = 0xfe
+      uint32.encode(state, n)
+    } else {
+      state.buffer[state.start++] = 0xff
+      uint64.encode(state, n)
+    }
+  },
+  decode (state) {
+    const a = uint8.decode(state)
+    if (a <= 0xfc) return a
+    if (a === 0xfd) return uint16.decode(state)
+    if (a === 0xfe) return uint32.decode(state)
+    return uint64.decode(state)
+  }
+}
+
+const uint8 = exports.uint8 = {
+  preencode (state, n) {
+    state.end += 1
+  },
+  encode (state, n) {
+    state.buffer[state.start++] = n
   },
   decode (state) {
     if (state.start >= state.end) throw new Error('Out of bounds')
-    const a = state.buffer[state.start++]
-    if (a <= 0xfc) return a
-    if (a === 0xfd) return decode16(state)
-    if (a === 0xfe) return decode32(state)
-    return decode64(state)
+    return state.buffer[state.start++]
   }
 }
 
-exports.int = {
-  preencode (state, n) {
-    uint.preencode(state, zigzagEncode(n))
-  },
-  encode (state, n) {
-    uint.encode(state, zigzagEncode(n))
-  },
-  decode (state) {
-    return zigzagDecode(uint.decode(state))
-  }
-}
-
-exports.uint16 = {
+const uint16 = exports.uint16 = {
   preencode (state, n) {
     state.end += 2
   },
@@ -49,11 +56,14 @@ exports.uint16 = {
   },
   decode (state) {
     if (state.end - state.start < 2) throw new Error('Out of bounds')
-    return state.buffer[state.start++] + state.buffer[state.start++] * 256
+    return (
+      state.buffer[state.start++] +
+      state.buffer[state.start++] * 256
+    )
   }
 }
 
-exports.uint24 = {
+const uint24 = exports.uint24 = {
   preencode (state, n) {
     state.end += 3
   },
@@ -64,11 +74,15 @@ exports.uint24 = {
   },
   decode (state) {
     if (state.end - state.start < 3) throw new Error('Out of bounds')
-    return state.buffer[state.start++] + state.buffer[state.start++] * 256 + state.buffer[state.start++] * 65536
+    return (
+      state.buffer[state.start++] +
+      state.buffer[state.start++] * 256 +
+      state.buffer[state.start++] * 65536
+    )
   }
 }
 
-exports.uint32 = {
+const uint32 = exports.uint32 = {
   preencode (state, n) {
     state.end += 4
   },
@@ -80,7 +94,51 @@ exports.uint32 = {
   },
   decode (state) {
     if (state.end - state.start < 4) throw new Error('Out of bounds')
-    return state.buffer[state.start++] + state.buffer[state.start++] * 256 + state.buffer[state.start++] * 65536 + state.buffer[state.start++] * 16777216
+    return (
+      state.buffer[state.start++] +
+      state.buffer[state.start++] * 256 +
+      state.buffer[state.start++] * 65536 +
+      state.buffer[state.start++] * 16777216
+    )
+  }
+}
+
+const uint64 = exports.uint64 = {
+  preencode (state, n) {
+    state.end += 8
+  },
+  encode (state, n) {
+    const r = Math.floor(n / 4294967296)
+    uint32.encode(state, n)
+    uint32.encode(state, r)
+  },
+  decode (state) {
+    if (state.end - state.start < 8) throw new Error('Out of bounds')
+    return uint32.decode(state) + 4294967296 * uint32.decode(state)
+  }
+}
+
+exports.int = zigZag(uint)
+exports.int8 = zigZag(uint8)
+exports.int16 = zigZag(uint16)
+exports.int24 = zigZag(uint24)
+exports.int32 = zigZag(uint32)
+exports.int64 = zigZag(uint64)
+
+exports.float32 = {
+  preencode (state, n) {
+    state.end += 4
+  },
+  encode (state, n) {
+    const view = new DataView(state.buffer.buffer, state.start + state.buffer.byteOffset, 4)
+    view.setFloat32(0, n, true) // little endian
+    state.start += 4
+  },
+  decode (state) {
+    const view = new DataView(state.buffer.buffer, state.start + state.buffer.byteOffset, 4)
+    const float = view.getFloat32(0, true) // little endian
+    state.start += 4
+    return float
   }
 }
 
@@ -206,40 +264,30 @@ exports.bool = {
     state.buffer[state.start++] = b ? 1 : 0
   },
   decode (state) {
-    if (state.start >= state.buffer.byteLength) throw Error('Out of bounds')
+    if (state.start >= state.end) throw Error('Out of bounds')
     return state.buffer[state.start++] === 1
   }
 }
 
-exports.fixed32 = {
-  preencode (state, h) {
-    state.end += 32
-  },
-  encode (state, h) {
-    state.buffer.set(h, state.start)
-    state.start += 32
-  },
-  decode (state) {
-    const b = state.buffer.subarray(state.start, state.start += 32)
-    if (b.length !== 32) throw new Error('Out of bounds')
-    return b
+const fixed = exports.fixed = function fixed (n) {
+  return {
+    preencode (state, s) {
+      state.end += n
+    },
+    encode (state, s) {
+      state.buffer.set(s, state.start)
+      state.start += n
+    },
+    decode (state) {
+      const b = state.buffer.subarray(state.start, state.start += n)
+      if (b.length !== n) throw new Error('Out of bounds')
+      return b
+    }
   }
 }
 
-exports.fixed64 = {
-  preencode (state, s) {
-    state.end += 64
-  },
-  encode (state, s) {
-    state.buffer.set(s, state.start)
-    state.start += 64
-  },
-  decode (state) {
-    const b = state.buffer.subarray(state.start, state.start += 64)
-    if (b.length !== 64) throw new Error('Out of bounds')
-    return b
-  }
-}
+exports.fixed32 = fixed(32)
+exports.fixed64 = fixed(64)
 
 exports.none = {
   preencode (state, m) {
@@ -253,23 +301,27 @@ exports.none = {
   }
 }
 
-exports.fixed = fixed
-exports.array = array
-exports.from = from
-
-exports.encode = function (enc, m) {
-  const state = { start: 0, end: 0, buffer: null }
-  enc.preencode(state, m)
-  state.buffer = b.allocUnsafe(state.end)
-  enc.encode(state, m)
-  return state.buffer
+exports.array = function array (enc) {
+  return {
+    preencode (state, list) {
+      uint.preencode(state, list.length)
+      for (let i = 0; i < list.length; i++) enc.preencode(state, list[i])
+    },
+    encode (state, list) {
+      uint.encode(state, list.length)
+      for (let i = 0; i < list.length; i++) enc.encode(state, list[i])
+    },
+    decode (state) {
+      const len = uint.decode(state)
+      if (len > 1048576) throw new Error('Array is too big')
+      const arr = new Array(len)
+      for (let i = 0; i < len; i++) arr[i] = enc.decode(state)
+      return arr
+    }
+  }
 }
 
-exports.decode = function (enc, buffer) {
-  return enc.decode({ start: 0, end: buffer.byteLength, buffer })
-}
-
-function from (enc) {
+exports.from = function from (enc) {
   if (enc.preencode) return enc
   if (enc.encodingLength) return fromAbstractEncoder(enc)
   return fromCodec(enc)
@@ -312,41 +364,16 @@ function fromAbstractEncoder (enc) {
   }
 }
 
-function array (enc) {
-  return {
-    preencode (state, list) {
-      uint.preencode(state, list.length)
-      for (let i = 0; i < list.length; i++) enc.preencode(state, list[i])
-    },
-    encode (state, list) {
-      uint.encode(state, list.length)
-      for (let i = 0; i < list.length; i++) enc.encode(state, list[i])
-    },
-    decode (state) {
-      const len = uint.decode(state)
-      if (len > 1048576) throw new Error('Array is too big')
-      const arr = new Array(len)
-      for (let i = 0; i < len; i++) arr[i] = enc.decode(state)
-      return arr
-    }
-  }
+exports.encode = function encode (enc, m) {
+  const state = { start: 0, end: 0, buffer: null }
+  enc.preencode(state, m)
+  state.buffer = b.allocUnsafe(state.end)
+  enc.encode(state, m)
+  return state.buffer
 }
 
-function fixed (n) {
-  return {
-    preencode (state, s) {
-      state.end += n
-    },
-    encode (state, s) {
-      state.buffer.set(s, state.start)
-      state.start += n
-    },
-    decode (state) {
-      const b = state.buffer.subarray(state.start, state.start += n)
-      if (b.length !== n) throw new Error('Out of bounds')
-      return b
-    }
-  }
+exports.decode = function decode (enc, buffer) {
+  return enc.decode({ start: 0, end: buffer.byteLength, buffer })
 }
 
 function LEToHost32 (arr, len) {
@@ -367,54 +394,25 @@ function hostToLE32 (arr, len) {
   }
 }
 
-function decode16 (state) {
-  if (state.start + 2 > state.end) throw new Error('Out of bounds')
-  return state.buffer[state.start++] + 256 * state.buffer[state.start++]
+function zigZag (enc) {
+  return {
+    preencode (state, n) {
+      enc.preencode(state, zigZagEncode(n))
+    },
+    encode (state, n) {
+      enc.encode(state, zigZagEncode(n))
+    },
+    decode (state) {
+      return zigZagDecode(enc.decode(state))
+    }
+  }
 }
 
-function encode16 (state, n) {
-  state.buffer[state.start++] = 0xfd
-  state.buffer[state.start++] = n
-  state.buffer[state.start++] = (n >>> 8)
-}
-
-function decode32 (state) {
-  if (state.start + 4 > state.end) throw new Error('Out of bounds')
-  return state.buffer[state.start++] + 256 * state.buffer[state.start++] +
-    65536 * state.buffer[state.start++] + 16777216 * state.buffer[state.start++]
-}
-
-function encode32 (state, n) {
-  state.buffer[state.start++] = 0xfe
-  state.buffer[state.start++] = n
-  state.buffer[state.start++] = (n = (n >>> 8))
-  state.buffer[state.start++] = (n = (n >>> 8))
-  state.buffer[state.start++] = (n >>> 8)
-}
-
-function decode64 (state) {
-  if (state.start + 8 > state.end) throw new Error('Out of bounds')
-  return decode32(state) + 4294967296 * decode32(state)
-}
-
-function encode64 (state, n) {
-  state.buffer[state.start++] = 0xff
-  let r = Math.floor(n / 4294967296)
-  state.buffer[state.start++] = n
-  state.buffer[state.start++] = (n = (n >>> 8))
-  state.buffer[state.start++] = (n = (n >>> 8))
-  state.buffer[state.start++] = (n >>> 8)
-  state.buffer[state.start++] = r
-  state.buffer[state.start++] = (r = (r >>> 8))
-  state.buffer[state.start++] = (r = (r >>> 8))
-  state.buffer[state.start++] = (r >>> 8)
-}
-
-function zigzagDecode (n) {
+function zigZagDecode (n) {
   return n === 0 ? n : (n & 1) === 0 ? n / 2 : -(n + 1) / 2
 }
 
-function zigzagEncode (n) {
+function zigZagEncode (n) {
   // 0, -1, 1, -2, 2, ...
   return n < 0 ? (2 * -n) - 1 : n === 0 ? 0 : 2 * n
 }
